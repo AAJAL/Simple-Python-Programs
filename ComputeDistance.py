570181c08b0ff1a18dd74df87d11a8bced5058ed
Aayan Jalal <33170152+AAJAL@users.noreply.github.com>
2020-07-15 12:49:09 -0700

    Add files via upload

---
+++ ComputeDistance.py
@@ -0,0 +1,9 @@
+
+def distance(x1, y1, x2, y2):
+    return ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) ** 0.5
+
+
+x1, y1 = eval(input("Enter x1 and y1 for Point 1: "))
+x2, y2 = eval(input("Enter x2 and y2 for Point 2: "))
+
+print("The distance between the two points is", distance(x1, y1, x2, y2))
